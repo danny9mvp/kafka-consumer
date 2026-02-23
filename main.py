0140e8b47f044be790a4b34fbbd9bd0d210f771f
danny9mvp <danny9mvp@gmail.com>
2026-02-23 12:52:33 -0500

    feature: created files for containerization

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import json
+import os
 from kafka import KafkaConsumer
 
-topic_name = 'danny-topic'
+
+BOOTSTRAP_SERVERS = os.getenv('BOOTSTRAP_SERVERS')
+TOPIC_NAME = os.getenv('TOPIC_NAME')
 consumer = KafkaConsumer(
-    topic_name,
-    bootstrap_servers=['localhost:9092'],
+    TOPIC_NAME,
+    bootstrap_servers=BOOTSTRAP_SERVERS,
     auto_offset_reset='earliest',
     value_deserializer=lambda m: json.loads(m.decode('utf-8')) if m else None,
 )
 
 
 if __name__ == '__main__':
-    print(f"Subscribed to topic {topic_name}")
+    print(f"Subscribed to topic {TOPIC_NAME}")
 
     for message in consumer:
         print(f"{message.value}")
